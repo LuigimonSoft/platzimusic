87ab924ac58a7b51076f0c16f673e0484af6aed8
Luis carlos carrillo <luigimon@Mac-Pro-de-Luis.local>
2018-04-05 17:32:26 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+  appname: "platzimusic",
+  apikey: "a6b5d1a7b717b6397994167bfb6a21f1",
+  secret: "19857104ec847e98cd73646a46d3d745",
+  registeredTo: "Luigimonsoft"
+};
+
+//Application name	platzimusic
+//API key	a6b5d1a7b717b6397994167bfb6a21f1
+//Shared secret	19857104ec847e98cd73646a46d3d745
+//Registered to	Luigimonsoft
